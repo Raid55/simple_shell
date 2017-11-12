7262b1ea7c839c4028ff311b41019507fc9dee22
Raid55 <boutboulnicholas@gmail.com>
2017-11-12 22:48:16 +0000

    added main file and header

---
+++ main.c
@@ -0,0 +1,9 @@
+/**
+ *
+ */
+int main(int ac, char **av, char **envp)
+{
+	_shell_init(envp);
+
+	return(0);
+}
---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+/*Standard Libs*/
+#include <stdio.h> /*for testing*/
+#include <stdlib.h> /*malloc, free, etc...*/
+#include <sys/wait.h> /*wait()*/
+#include <unistd.h> /*system libraries*/
+#include <string.h> /*string manip function*/
+#include <sys/types.h> /*datatypes for syscall returns*/
+#include <sys/stat.h> /*I need a detective to investigate this...STAT*/
+#include <fcntl.h> /*fork system */
+
+/*Main Functions Prototypes*/
+void _shell_init(char **);
+void _shell_instance(char **);
+
+/*Side Functions Prototypes*/
+void exec_process(char *, char **);
+char **tokeniser(char **);
+
+#endif
