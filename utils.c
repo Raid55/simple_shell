5c8f60e60e6fe698ce53065e5fc98253746b4eac
Raid55 <boutboulnicholas@gmail.com>
2017-11-13 02:35:53 +0000

    removed old code

--- utils.c
+++ utils.c
@@ -89,34 +89,6 @@ char *get_x_path(char **env_paths, char *program)
 	else
 		return (tmp);
 }
-/* void  _get_x_path(char **env_paths, char *program, char **tmp_path) */
-/* { */
-/*         int i = 1; */
-/*         char *tmp; */
-/*         int pCount = strlen(program); */
-
-/*         tmp = malloc(sizeof(char) * (strlen(env_paths[0]) + pCount) + 1); */
-/*         if (tmp == NULL) */
-/*                 perror("grosse error alert rouge, segfult!: "), exit(-1); */
-/*         tmp[0] = '\0'; */
-/*         strcpy(tmp, env_paths[0]); */
-/*         strcat(tmp, program); */
-/*         tmp[strlen(env_paths[0]) + pCount] = '\0'; */
-/*         while (access(tmp, X_OK) == -1 || env_paths[i] != NULL) */
-/*         { */
-/*                 tmp = realloc(tmp, sizeof(char) * (strlen(env_paths[i]) + pCount) + 1); */
-/*                 if (tmp == NULL) */
-/*                         perror("due gros problem, segbut!!!: "), exit(-1); */
-/*                 tmp[0] = '\0'; */
-/*                 strcpy(tmp, env_paths[i]); */
-/*                 strcat(tmp, program); */
-/*                 tmp[strlen(env_paths[i] + pCount)] = '\0', i++; */
-/*         } */
-/*         if (env_paths[i] == NULL) */
-/*                 free(tmp), *tmp_path = NULL; */
-/*         else */
-/*                 *tmp_path = tmp; */
-/* } */
 /**
  *
  *
